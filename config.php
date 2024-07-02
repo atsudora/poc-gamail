1259c61551dd4597178c698cf8eb7c79ff4f054e
atsudora <atsudorasan@gmail.com>
2024-07-02 23:18:05 +0900

    test

--- config.php
+++ config.php
@@ -13,7 +13,7 @@ return [
         'from_name' => 'Mailer',
         'subject' => 'Test Email for Google Analytics',
         'tracking_pixel_url' => 'https://poc-gamail.onrender.com/track_open.php',
-        'google_analytics_url' => 'https://poc-gamail.onrender.com/complete.html',
+        'google_analytics_url' => 'https://poc-gamail.onrender.com/',
         'utm_parameters' => 'utm_source=arara&utm_medium=email&utm_campaign=test_campaign'
     ],
     'ga_tracking_id' => 'G-83RQZRK36V',
